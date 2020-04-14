4aa3376c1a36074742496b63ec4fdce720cc193a
Himanshusinghofficial <hs882677@gmail.com>
2020-04-14 22:16:28 +0530

    change time

--- src/forecast.js
+++ src/forecast.js
@@ -10,17 +10,20 @@ const forecast=(latitude,longitude,callback)=>{
         callback('unable to find the location',undefined)
         }
         else{
-            var date=new Date((response.body.dt*1000)+19800).toLocaleString();
-            var sunrise=new Date((response.body.dt*1000)+19800).toLocaleTimeString();
-            var sunset=new Date((response.body.dt*1000)+19800).toLocaleTimeString();
+            var date=new Date((response.body.dt*1000)+19800)
+                 var dateval=date.toString();
+            var sunrise=new Date((response.body.sys.sunrise*1000)+19800)
+            sunriseval=sunrise.toTimeString();
+            var sunset=new Date((response.body.sys.sunset*1000)+19800)
+            sunsetval=sunset.toTimeString();
              
         //  callback(undefined, 'Weather condition '+response.body.weather[0].main + ' It is currently ' + (response.body.main.temp) +' deg C' +'                                                                                       Date:'+  date + '                                                                                                               sunrise:' + sunrise +'                                                                                                                 sunset:'+sunset)
          callback(undefined,{
              condition:response.body.weather[0].main,
              temp:response.body.main.temp,
-             date:date,
-             sunrise:sunrise,
-             sunset:sunset
+             date:dateval,
+             sunrise:sunriseval,
+             sunset:sunsetval
 
          })
         }
